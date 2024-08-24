7174483adc824b82d69fe141e04909d83e9f8fb1
scastillo <sergio.castillo@ucreativa.com>
2024-08-24 17:44:23 -0600

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const User = require('./models/User');
 
 const app = express();
-const PORT = 3001;
+const PORT = 4001;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
