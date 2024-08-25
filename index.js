ce3bcaa12717ed4b24743988aaea44f4e2da5000
scastilloU <134464125+scastilloU@users.noreply.github.com>
2024-08-24 20:56:53 -0600

    Update index.js
    The new port 5001 is ready for prod id123456789

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const User = require('./models/User');
 
 const app = express();
-const PORT = 4001;
+const PORT = 5001;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
